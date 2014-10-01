7291586ef46e384dc37c699fb25e96c3d864543b
PRMenon <pratheek.menon@gmail.com>
2014-10-01 19:31:11 -0400

    Basic Player Movement
    Added movement script

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+
+	public Vector3 speed = new Vector3(50, 50, 0);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		// 4 - Movement per direction
+		movement = new Vector3(
+			speed.x * inputX,
+			speed.y * inputY,
+			0				);
+			
+		
+	}
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody.velocity = movement;
+	}
+}
